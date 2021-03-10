afaa227f3a224c0881aa70777ea5136dd27b7407
Alexander Nesterov <alex19pov31@gmail.com>
2021-03-10 16:28:58 +0300

    update search field in admin ui

--- lib/ui/fields/searchfilterfield.php
+++ lib/ui/fields/searchfilterfield.php
@@ -11,16 +11,21 @@ class SearchFilterField extends StringFilterField
     public function toArray(): array
     {
         return [
-                "id" => $this->id,
-                "name" => $this->title,
-                "filterable" => "?",
-                "quickSearch" => "?",
-                "default" => $this->isDefault,
-            ];
+            "id" => $this->id,
+            "name" => $this->title,
+            "filterable" => "?",
+            "quickSearch" => "?",
+            "default" => $this->isDefault,
+        ];
     }
 
     public function getFilterField(Options $options): array
     {
+        $find = $options->getFilter()['FIND'] ?? null;
+        if (!empty($find)) {
+            return ['like_'.$this->id => $find];
+        }
+
         return parent::getFilterField($options);
     }
 }
