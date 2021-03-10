0c13a2fbc8070619bd28f589f110056031b3f263
Alexander Nesterov <alex19pov31@gmail.com>
2021-03-10 16:17:09 +0300

    update number filter in admmin ui

--- lib/ui/fields/numberfilterfield.php
+++ lib/ui/fields/numberfilterfield.php
@@ -21,12 +21,49 @@ class NumberFilterField extends BaseFilterField
     public function getFilterField(Options $options): array
     {
         $data = $this->getOptionsFilter($options);
-        if (!empty($data[$this->id])) {
-            return [
-                $this->id => $data[$this->id],
-            ];
-        }
+        $from = $data[$this->id.'_from'] ?? null;
+        $to = $data[$this->id.'_to'] ?? null;
+        $mFrom = (int)$from+1;
+        $mTo = (int)$to-1;
 
-        return [];
+        $type = $data[$this->id.'_numsel'] ?? null;
+        switch ($type) {
+            case 'range':
+                $filter = [];
+                if (!empty($from)) {
+                    $filter['from_'.$this->id] = $mFrom;
+                }
+
+                if (!empty($to)) {
+                    $filter['to_'.$this->id] = $mTo;
+                }
+
+                return $filter;
+            case 'more':
+                $filter = [];
+                if (!empty($from)) {
+                    $filter['from_'.$this->id] = $mFrom;
+                }
+
+                return $filter;
+            case 'less':
+                $filter = [];
+                if (!empty($to)) {
+                    $filter['to_'.$this->id] = $mTo;
+                }
+
+                return $filter;
+            default:
+                $filter = [];
+                if (!empty($from)) {
+                    $filter[$this->id] = $mFrom;
+                }
+
+                if (!empty($to)) {
+                    $filter[$this->id] = $mTo;
+                }
+
+                return $filter;
+        }
     }
 }
